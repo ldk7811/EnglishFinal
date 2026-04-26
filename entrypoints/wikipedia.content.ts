9baa0ca83e73fdbb3914b13fc478520231077e3e
Luke Knofczynski <ldk7811@rit.edu>
2026-04-26 15:38:50 -0400

    Add everything to git

---
+++ entrypoints/wikipedia.content.ts
@@ -0,0 +1,34 @@
+import "./style.css"
+
+export default defineContentScript({
+  matches: ["*://*.wikipedia.org/*"],
+  main() {
+    console.log("Hi wikipedia");
+    const attachListener = () => {
+      const links = document.querySelectorAll<HTMLAnchorElement>(
+        "#bodyContent p a, #bodyContent li a",
+      );
+
+      links.forEach((link) => {
+        link.addEventListener("mouseenter", reveal);
+        link.addEventListener("focus", reveal);
+      });
+    };
+
+    const reveal = (e: Event) => {
+        const target = e.target as HTMLElement;
+        const parent = target.closest("p, li");
+        if (parent) {
+            document.querySelectorAll(".hypertext-revealed").forEach(el => {
+                el.classList.remove("hypertext-revealed");
+            });
+            parent.classList.add("hypertext-revealed")
+        }
+    }
+    if (document.readyState === "loading") {
+        document.addEventListener("DOMContentLoaded", attachListener);
+    } else {
+        attachListener();
+    }
+  },
+});
---
+++ wxt.config.ts
@@ -0,0 +1,11 @@
+import { defineConfig } from 'wxt';
+
+// See https://wxt.dev/api/config.html
+export default defineConfig({
+    manifest: {
+        name: "Hypertext Attention",
+        description: "Makes wikipedia viewable only by hyperlinks",
+        version: "0.1.0",
+        permissions: []
+    }
+});
